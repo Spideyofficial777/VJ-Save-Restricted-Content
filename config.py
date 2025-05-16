3948787c4fec0ebb326598b38edcab69c3a43c8c
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-16 13:10:19 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+from os import path, getenv, environ
 
 # Bot token @Botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
@@ -19,3 +19,18 @@ DB_NAME = os.environ.get("DB_NAME", "spideysavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+
+#LOG CHANNEL for the user logs
+LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1002423451263"))
+
+#imgs
+START_IMG = (environ.get('START_IMG', 'https://graph.org/file/2518d4eb8c88f8f669f4c.jpg https://graph.org/file/d6d9d9b8d2dc779c49572.jpg https://graph.org/file/4b04eaad1e75e13e6dc08.jpg https://graph.org/file/05066f124a4ac500f8d91.jpg https://graph.org/file/2c64ed483c8fcf2bab7dd.jpg https://i.ibb.co/CPxdkHR/IMG-20240818-192201-633.jpg')).split()
+
+#for the forceSub 
+CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1002470391435,-1002433552221").split(",")))
+
+#temp
+class temp(object):    
+    U_NAME = None
+    B_NAME = None
+    MELCOW = {} 
