9ada4d2a18de8f2f34cbcc1d52003c5faff938b3
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-16 13:48:13 +0530

    Add files via upload

--- Spidey/start.py
+++ Spidey/start.py
@@ -8,7 +8,7 @@ import pyrogram
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
-from config import API_ID, API_HASH, ERROR_MESSAGE
+from config import * # API_ID, API_HASH, ERROR_MESSAGE
 from database.db import db
 from Spidey.strings import HELP_TXT
 from Script import script
