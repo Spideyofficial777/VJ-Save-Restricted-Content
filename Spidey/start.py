f9afa91b2a9580166483cd2f1e2b8d5a79a92dac
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-16 13:59:11 +0530

    Add files via upload

--- Spidey/start.py
+++ Spidey/start.py
@@ -14,6 +14,8 @@ from Spidey.strings import HELP_TXT
 from Script import script
 from pyrogram.errors import UserNotParticipant
 from database.db import * # get_all_users, add_user, already_db
+from utils import* #
+
 
 class batch_temp(object):
     IS_BATCH = {}
