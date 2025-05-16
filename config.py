4bef8bc3a522b7ea323fe141d2b300fa8d3a2361
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-16 23:05:47 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -26,6 +26,7 @@ LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1002423451263"))
 
 #imgs
 START_IMG = (environ.get('START_IMG', 'https://graph.org/file/2518d4eb8c88f8f669f4c.jpg https://graph.org/file/d6d9d9b8d2dc779c49572.jpg https://graph.org/file/4b04eaad1e75e13e6dc08.jpg https://graph.org/file/05066f124a4ac500f8d91.jpg https://graph.org/file/2c64ed483c8fcf2bab7dd.jpg https://i.ibb.co/CPxdkHR/IMG-20240818-192201-633.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://envs.sh/v3t.jpg')).split()
 
 #for the forceSub 
 CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1002470391435,-1002433552221").split(",")))
